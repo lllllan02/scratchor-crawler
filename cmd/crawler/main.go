f812b180638ad3bb85d8cb47a5e90735377e088e
lllllan <342310798@qq.com>
2025-07-22 13:37:41 +0800

    重构爬虫主逻辑，将原有的main.go文件内容迁移至cmd/crawler/main.go，新增爬虫结构体及相关方法，优化分类和章节的爬取逻辑，支持从已存在的最大ID继续爬取，提升代码结构和可维护性。

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -1,4 +1,4 @@
-package crawler
+package main
 
 import (
 	"bytes"
@@ -268,4 +268,14 @@ func (c *Crawler) View(path, url string) error {
 	fmt.Printf("题目详情保存成功: %s (大小: %d 字节)\n", filePath, len(data))
 
 	return nil
+}
+
+func main() {
+	craler, err := NewCrawler("9c8adef9772544421fc21404af7ed346=e6f5696bd5ff493578540e8afee89af8; ssid=eyJpdiI6IjBRYUlUYzdNeFM4RGhjZ2pOT0RNWnc9PSIsInZhbHVlIjoiZUUzSW5NcTU0bFwvaVYwRFwvaVFIRXhzZldUYWFtcVdVMkpETlZKQmxTb1lkaWNKSGxmT05lVmdNMjA1XC8rQVZhXC84SjY1TG5aWGJRSGlkU0lUblJaZjhBPT0iLCJtYWMiOiI3YWVmYTk0NDQxZjg3YTFiMDlkYjhlNWUwMDNmMzFhM2FkNjBmZWY0Yzg1OWVhYjQ1OTQ0M2E3ODRhNjkzMzI0In0%3D")
+
+	if err != nil {
+		panic(err)
+	}
+
+	craler.Cats()
 }
